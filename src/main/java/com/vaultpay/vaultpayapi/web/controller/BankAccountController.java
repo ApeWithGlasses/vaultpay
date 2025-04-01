f22d3eb6ef015cbd25fe29a15963e902b5b1e925
ApeWithGlasses <wcgiraldo0@gmail.com>
2025-04-01 15:47:46 -0500

    jsp views added

--- src/main/java/com/vaultpay/vaultpayapi/web/controller/BankAccountController.java
+++ src/main/java/com/vaultpay/vaultpayapi/web/controller/BankAccountController.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/api/accounts")
 @RequiredArgsConstructor
 public class BankAccountController {
     private final BankAccountService bankAccountService;
